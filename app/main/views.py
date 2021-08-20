572bc6fa37681b5a63ffe5a7eeec78324a988945
leparwa <kevin.leparwa@student.moringaschool.com>
2021-08-20 20:46:20 +0300

    user auth

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
+from flask.templating import render_template
 from flask_login import login_required
+from flask import render_template
 from . import main
 
 @main.route('/', methods = ['GET','POST'])
 @login_required
-def new_review():
-    pass
+def user_profile():
+    return render_template('user_profile.html')
